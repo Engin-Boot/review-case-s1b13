43f5b68ba810e5ff437ca9f61b77d2bb0eb5beeb
Chandan N <49569507+chandangowda7@users.noreply.github.com>
2020-09-09 19:45:29 +0530

    Update receiver.cpp

--- receiver/receiver.cpp
+++ receiver/receiver.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 map<std::string,int> countWords(map<std::string,int>,string );
 void printwordandwordcounts(map<std::string,int>);
+map<string,int> InsertWords(map<string,int>,vector<string>);
 map<string, int> InsertlastWord(map<string, int>,string);
 bool ifwordfound(map<string, int>,string) ;
 int main()
@@ -35,7 +36,7 @@ void printwordandwordcounts(map<std::string,int> M)
 map<std::string,int> countWords(map<std::string,int> M,string line)
 {
 // String for storing the words 
-    string word = ""; 
+    string word = ""; vector<string> words;
   
     for (int i = 0;line[i]!='\0'; i++) { 
   
@@ -45,7 +46,7 @@ map<std::string,int> countWords(map<std::string,int> M,string line)
             // If the current word 
             // is not found then insert 
             // current word with frequency 1 
-            if (ifwordfound(M,word) { 
+            /*if (ifwordfound(M,word) { 
                 M.insert(make_pair(word, 1)); 
                 word = ""; 
             } 
@@ -54,24 +55,32 @@ map<std::string,int> countWords(map<std::string,int> M,string line)
             else { 
                 M[word]++; 
                 word = ""; 
-            } 
+            } */
+         words.push_back(word);
+         word="";
         } 
   
         else
             word += line[i]; 
     } 
+ words.pushback(word);
    
- return InsertlastWord(M,word);
+ return InsertWords(M,words);
 }
-bool ifwordfound(map<string,int> M,string word) 
+
+map<string,int> InsertWords(map<string,int> M,vector<string> words) 
 { 
     // Key is not present 
+ for(int i=0;i<words.size();i++){
     if (M.find(word) == M.end()) 
-        return true; 
+     M.insert(make_pair(word, 1)); 
+  else
+   M[word]++;
+        
   
-    return false; 
+    return M; 
 }
-map<string, int> InsertlastWord(map<string, int> M,string word)
+/*map<string, int> InsertlastWord(map<string, int> M,string word)
 {
  if (M.find(word) == M.end()) 
         M.insert(make_pair(word, 1)); 
@@ -80,4 +89,4 @@ map<string, int> InsertlastWord(map<string, int> M,string word)
     else
         M[word]++; 
  return M;
-}
+}*/
