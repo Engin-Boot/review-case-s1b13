4fe0e5c216dc8e62b062af3b0c179e3ea652be3d
Chandan N <49569507+chandangowda7@users.noreply.github.com>
2020-09-09 17:06:28 +0530

    Update Receiver.cpp

--- Receiver.cpp
+++ Receiver.cpp
@@ -1,11 +1,33 @@
 #include<fstream>
 #include<iostream>
+#include<map>
 using namespace std;
-int main(int argc,char *argv[])
+int main
 {
-    cout<<"Passed"<<argc<<"arguments!"<<endl;
-    for (int i = 0; i < argc; ++i) 
-        cout << argv[i] << "\n"; 
-  
-    return 0;
+std::unordered_map<string, int> wordFreq;
+string line="";
+while(getline(cin,line))
+{
+if(line=="")
+{
+break;
 }
+storeFrequencies(line,wordFreq);
+
+}
+ unordered_map<string, int>:: iterator p; 
+ for (p = wordFreq.begin(); p != wordFreq.end(); p++) 
+        cout << "(" << p->first << ", " << p->second << ")\n"; 
+
+}
+void storeFrequencies(const string &str,std::unordered_map<string, int> wordFreq) 
+{ 
+  
+  
+   
+    stringstream ss(str); 
+    string word;
+    while (ss >> word) 
+        wordFreq[word]++; 
+   
+} 
