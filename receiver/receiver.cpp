f02d50c4677172c443a845065d08e50ec841b90b
Chandan N <49569507+chandangowda7@users.noreply.github.com>
2020-09-11 18:17:42 +0530

    Update receiver.cpp

--- receiver/receiver.cpp
+++ receiver/receiver.cpp
@@ -1,105 +1,71 @@
 
-#include<fstream>
-#include<iostream>
-#include<map>
-#include<vector>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <vector>
 using namespace std;
 
-map<std::string,int> countWords(map<std::string,int>&,string );
-void printwordandwordcounts(map<std::string,int>);
-map<string,int> InsertWords(map<string,int>,std::vector<string>);
-//map<string, int> InsertlastWord(map<string, int>,string);
-//bool ifwordfound(map<string, int>,string) ;
+map<std::string, int> countWords(map<std::string, int>&, string);
+void printwordandwordcounts(map<std::string, int>);
+map<string, int> InsertWords(map<string, int>, std::vector<string>);
+
 int main()
 {
- map<string, int> M; 
-string line="";
-while(getline(cin,line))
-{
-if(line=="")
-{
-break;
-}
- map<string, int> tempMap=countWords(M,line);
-  M=tempMap;
-    
-}
- 
- printwordandwordcounts(M);
-return 0;
-}
-void printwordandwordcounts(map<std::string,int> M)
-{
- 
- if(M.size()>0)
- {
-  fstream fout;
- fout.open("WordCount.csv", ios::out | ios::app);
- for (auto& it : M)
- { 
-        fout << it.first << " - "
-             << it.second 
-             << endl; 
+    map<string, int> M;
+    string line = "";
+    while (getline(cin, line)) {
+        if (line == "") {
+            break;
+        }
+        map<string, int> tempMap = countWords(M, line);
+        M = tempMap;
     }
- }
- else
-  cout<<"Empty Input Received";
+    printwordandwordcounts(M);
+    return 0;
 }
-map<std::string,int> countWords(map<std::string,int> &M,string line)
+void printwordandwordcounts(map<std::string, int> M)
 {
-// String for storing the words 
-    string word = ""; std::vector<string> words;
-  
-    for (int i = 0;line[i]!='\0'; i++) { 
-  
-     
-        if (line[i] == ' ') { 
-  
-            // If the current word 
-            // is not found then insert 
-            // current word with frequency 1 
-            /*if (ifwordfound(M,word) { 
-                M.insert(make_pair(word, 1)); 
-                word = ""; 
-            } 
-  
-            // update the frequency 
-            else { 
-                M[word]++; 
-                word = ""; 
-            } */
-         words.push_back(word);
-         word="";
-        } 
-  
-        else
-            word += line[i]; 
-    } 
- words.push_back(word);
-   
- return InsertWords(M,words);
-}
 
-map<string,int> InsertWords(map<string,int> M,std::vector<string> words) 
-{ 
-    // Key is not present 
- for (auto& word : words){
-    if (M.find(word) == M.end()) 
-     M.insert(make_pair(word, 1)); 
-  else
-   M[word]++;
- }  
-  
-    return M; 
-}
-
-/*map<string, int> InsertlastWord(map<string, int> M,string word)
-{
- if (M.find(word) == M.end()) 
-        M.insert(make_pair(word, 1)); 
-  
-    // Update the frequency 
+    if (M.size() > 0) {
+        fstream fout;
+        fout.open("sample-review/WordCount.csv", ios::out | ios::app);
+        for (auto& it : M) {
+            fout << it.first << " - "
+                 << it.second
+                 << endl;
+        }
+    }
     else
-        M[word]++; 
- return M;
-}*/
+        cout << "Empty Input Received";
+}
+map<std::string, int> countWords(map<std::string, int>& M, string line)
+{
+    // String for storing the words
+    string word = "";
+    std::vector<string> words;
+
+    for (int i = 0; line[i] != '\0'; i++) {
+
+        if (line[i] == ' ') {
+            words.push_back(word);
+            word = "";
+        }
+        else
+            word += line[i];
+    }
+    words.push_back(word);
+
+    return InsertWords(M, words);
+}
+
+map<string, int> InsertWords(map<string, int> M, std::vector<string> words)
+{
+    for (auto& word : words) {
+        if (M.find(word) == M.end())
+            M.insert(make_pair(word, 1));
+        else
+            M[word]++;
+    }
+
+    return M;
+}
