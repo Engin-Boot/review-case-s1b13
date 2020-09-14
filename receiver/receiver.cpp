8dda180aed40c25173b4cd83f4d5fa517f43041c
Chandan N <49569507+chandangowda7@users.noreply.github.com>
2020-09-14 15:29:49 +0530

    Update receiver.cpp

--- receiver/receiver.cpp
+++ receiver/receiver.cpp
@@ -3,12 +3,12 @@
 
 using namespace std;
 
-map<std::string, int> countWords(map<std::string, int>&, string);
+/*map<std::string, int> countWords(map<std::string, int>&, string);
 void printwordandwordcounts(map<std::string, int>);
 map<string, int> InsertWords(map<string, int>, std::vector<string>);
-vector<string> checkforalphabets(string,vector<string>);
+vector<string> checkforalphabets(string,vector<string>);*/
 
-int main()
+int Receiver::main()
 {
     map<string, int> M;
     string line = "";
@@ -22,7 +22,7 @@ int main()
     printwordandwordcounts(M);
     return 0;
 }
-void printwordandwordcounts(map<std::string, int> M)
+void Receiver::printwordandwordcounts(map<std::string, int> M)
 {
 
     if (M.size() > 0) {
@@ -35,7 +35,7 @@ void printwordandwordcounts(map<std::string, int> M)
     else
         cout << "Empty Input Received";
 }
-map<std::string, int> countWords(map<std::string, int>& M, string line)
+map<std::string, int> Receiver::countWords(map<std::string, int>& M, string line)
 {
     // String for storing the words
     string word = "";
@@ -56,7 +56,7 @@ map<std::string, int> countWords(map<std::string, int>& M, string line)
     return InsertWords(M, words);
 }
 
-map<string, int> InsertWords(map<string, int> M, std::vector<string> words)
+map<string, int> Receiver::InsertWords(map<string, int> M, std::vector<string> words)
 {
     for (auto& word : words) {
         if (M.find(word) == M.end())
@@ -67,7 +67,7 @@ map<string, int> InsertWords(map<string, int> M, std::vector<string> words)
 
     return M;
 }
-vector<string> checkforalphabets(string word,vector<string> words)
+vector<string> Receiver::checkforalphabets(string word,vector<string> words)
 {
     int i=0;
 while(word[i])
