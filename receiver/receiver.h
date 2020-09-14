2a1b347c85a8e88b114e884c325bc8b0a29e5e26
Chandan N <49569507+chandangowda7@users.noreply.github.com>
2020-09-14 14:53:15 +0530

    Create receiver.h

---
+++ receiver/receiver.h
@@ -0,0 +1,11 @@
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <vector>
+namespce receiver
+{
+map<std::string, int> countWords(map<std::string, int>&, string);
+void printwordandwordcounts(map<std::string, int>);
+map<string, int> InsertWords(map<string, int>, std::vector<string>);
+vector<string> checkforalphabets(string,vector<string>);
+}
