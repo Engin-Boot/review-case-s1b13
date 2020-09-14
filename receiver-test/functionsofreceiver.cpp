0c975466436ed7df543fb31f20e022ef28823d80
Chandan N <49569507+chandangowda7@users.noreply.github.com>
2020-09-14 20:35:59 +0530

    Create functionsofreceiver.cpp

---
+++ receiver-test/functionsofreceiver.cpp
@@ -0,0 +1,64 @@
+#include "../receiver/receiver.h"
+
+using namespace std;
+
+
+
+void Receiver::printwordandwordcounts(map<std::string, int> M)
+{
+
+    if (M.size() > 0) {
+        fstream fout;
+        fout.open("sample-review/WordCount.csv", ios::out | ios::app);
+        for (auto& iterator : M) {
+            fout << iterator.first << "," << iterator.second << "\n";
+        }
+    }
+    else
+        cout << "Empty Input Received";
+}
+map<std::string, int> Receiver::countWords(map<std::string, int>& M, string line)
+{
+    // String for storing the words
+    string word = "";
+    std::vector<string> words,tempwords;
+
+    for (int i = 0; line[i] != '\0'; i++) {
+
+        if (line[i] == ' ') {
+            tempwords=Receiver::checkforalphabets(word,words);
+            words=tempwords;
+            word = "";
+        }
+        else
+            word += line[i];
+    }
+    tempwords=Receiver::checkforalphabets(word,words);
+     words=tempwords;
+    return Receiver::InsertWords(M, words);
+}
+
+map<string, int> Receiver::InsertWords(map<string, int> M, std::vector<string> words)
+{
+    for (auto& word : words) {
+        if (M.find(word) == M.end())
+            M.insert(make_pair(word, 1));
+        else
+            M[word]++;
+    }
+
+    return M;
+}
+vector<string> Receiver::checkforalphabets(string word,vector<string> words)
+{
+    int i=0;
+while(word[i])
+{
+    if (!isalpha(word[i]))
+        return words;
+    i++;
+  
+}
+    words.push_back(word);
+    return words;
+}
