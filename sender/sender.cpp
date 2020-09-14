572ed12b99355a6592c0043d51085164f4b21be5
RachotiBiradar <44144000+RachotiBiradar@users.noreply.github.com>
2020-09-14 11:37:14 +0530

    Update sender.cpp

--- sender/sender.cpp
+++ sender/sender.cpp
@@ -3,21 +3,43 @@
 #include <fstream>
 #include <sstream>
 #include <algorithm> 
-#include <functional> 
-#include <cctype>
-#include <locale>
-
+#include<sys/stat.h>
 
 using namespace std;
 void IfNoArgumentsPassed(fstream&);
 void IfArgumentsPassed(fstream&  ,char**);
 void CommaCounter(string ,int);
 void PrintSelectedColumn(int ,int ,string ,int);
-
+inline bool exists(const std::string& filename){
+  struct stat buffer;   
+  return (stat (filename.c_str(), &buffer) == 0); 
+}
+void CheckIfTheFileExists(string& FileName)
+{
+    bool exist = exists(FileName);
+    if(exist == false)
+    {
+        cout<<"File does not exists"<<endl;
+        exit(0);
+    }
+    
+}
+void isFileOpen(fstream& fin,string& fileName)
+{
+    fin.open(fileName.c_str(),ios::in);
+	if(!fin.is_open())
+    {
+        cout << "File cannot be opened" << endl;
+		exit(0);
+    }
+}
 int main(int argc,char *argv[])
 {
     fstream fin; 
-    fin.open("sample-review/review-report.csv", ios::in);
+    string FileName = "sample-review/review-report.csv";
+    CheckIfTheFileExists(FileName);
+    isFileOpen(fin,FileName);
+    
     if(argc==1)
     {
         IfNoArgumentsPassed(fin);
