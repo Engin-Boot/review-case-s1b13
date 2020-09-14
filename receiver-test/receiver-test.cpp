0b7b9c26c4b412250bccac5c033a62ca761d39ad
Chandan N <49569507+chandangowda7@users.noreply.github.com>
2020-09-14 20:18:31 +0530

    Create receiver-test.cpp

---
+++ receiver-test/receiver-test.cpp
@@ -0,0 +1,20 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include <iostream>
+#include <map>
+#include <vector>
+#include "../receiver/receiver.h"
+#include "../catch.hpp"
+
+using namespace std;
+TEST_CASE("printing the word and word count") {
+ map<std::string, int> M;
+   M.insert(make_pair("one", 1));
+  std::vector<string> words={"one","two"};
+  std::map<std::string, int> result,res2;
+  result.insert(make_pair("one", 2));
+  result.insert(make_pair("two", 1));
+  res2=Receiver::InsertWords(M,words);
+  
+    REQUIRE(res2==result);
+    
+}
