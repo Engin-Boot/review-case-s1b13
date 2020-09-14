3aa829aa15c0597c4dfc16c53cd50bc190de45c2
Chandan N <49569507+chandangowda7@users.noreply.github.com>
2020-09-14 12:00:43 +0530

    Update receiver.cpp

--- receiver/receiver.cpp
+++ receiver/receiver.cpp
@@ -8,6 +8,7 @@ using namespace std;
 map<std::string, int> countWords(map<std::string, int>&, string);
 void printwordandwordcounts(map<std::string, int>);
 map<string, int> InsertWords(map<string, int>, std::vector<string>);
+vector<string> checkforalphabets(string,vector<string>);
 
 int main()
 {
@@ -40,19 +41,20 @@ map<std::string, int> countWords(map<std::string, int>& M, string line)
 {
     // String for storing the words
     string word = "";
-    std::vector<string> words;
+    std::vector<string> words,tempwords;
 
     for (int i = 0; line[i] != '\0'; i++) {
 
         if (line[i] == ' ') {
-            words.push_back(word);
+            tempwords=checkforalphabets(word,words);
+            words=tempwords;
             word = "";
         }
         else
             word += line[i];
     }
-    words.push_back(word);
-
+    tempwords=checkforalphabets(word,words);
+     words=tempwords;
     return InsertWords(M, words);
 }
 
@@ -66,4 +68,16 @@ map<string, int> InsertWords(map<string, int> M, std::vector<string> words)
     }
 
     return M;
+}
+vector<string> checkforalphabets(string word,vector<string> words)
+{
+    int i=0;
+while(word[i])
+{
+    if (!isalpha(str[i]))
+        return words;
+    else
+        words.push_back(word); 
+}
+    return words;
 }
