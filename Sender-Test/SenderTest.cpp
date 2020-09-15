7d071a6c8248f6d4315f025aea1fb74b19b3e643
RachotiBiradar <44144000+RachotiBiradar@users.noreply.github.com>
2020-09-15 18:00:29 +0530

    Update SenderTest.cpp

--- Sender-Test/SenderTest.cpp
+++ Sender-Test/SenderTest.cpp
@@ -9,16 +9,44 @@
 
 using namespace std;
 Sender s;
-
-TEST_CASE("This is the First CASE")
+inline bool exists(const std::string& filename){
+  struct stat buffer;   
+  return (stat (filename.c_str(), &buffer) == 0); 
+}
+bool CheckIfTheFileExists(string& FileName)
 {
-    
-    string third = "10/23/45 23:45,No additional comments";
-    
-    
-    char data1 = s.PrintSelectedColumn(1,2,third,15);
-    cout<<data1<<endl;
-    char data = 'N';
-    REQUIRE(data1==data);
+    bool exist = exists(FileName);
+    if(exist == false)
+    {
+        cout<<"File does not exists"<<endl;
+        
+    }
+    return exist;
 }
 
+TEST_CASE("WHEN we pass to function a CSV line containing date & comment and if we need comment part THEN it returns Comment part")
+{    
+    string ExampleCSVString = "10/23/45 23:45,No additional comments";    
+    string return_string = s.CommaCounter(ExampleCSVString,1);    
+    REQUIRE(return_string == "No additional comments");
+}
+TEST_CASE("WHEN we pass to function a CSV line containing date & comment and if we need date part THEN it returns date part")
+{    
+    string ExampleCSVString = "10/23/45 23:45,No additional comments";    
+    string return_string = s.CommaCounter(ExampleCSVString,0);    
+    REQUIRE(return_string == "10/23/45 23:45");
+}
+TEST_CASE("WHEN we pass the filename to the function THEN it returns if the file exists or not")
+{
+    string FileName = "sample-review/review-report.csv";
+    bool FileExists = CheckIfTheFileExists(FileName);
+    REQUIRE(FileExists == true);
+}
+TEST_CASE("WHEN we pass the filename to the function THEN it returns if the file  not")
+{
+    string FileName = "sample-review/review-report1.csv";
+    bool FileExists = CheckIfTheFileExists(FileName);
+    REQUIRE(FileExists == false);
+}
+
+ 
