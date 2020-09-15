ac0ac8b3c7a9dea81a8ecaf849aef5d9048c0bad
RachotiBiradar <44144000+RachotiBiradar@users.noreply.github.com>
2020-09-15 11:37:25 +0530

    Create SenderTest.cpp

---
+++ Sender-Test/SenderTest.cpp
@@ -0,0 +1,24 @@
+#define CATCH_CONFIG_MAIN  
+#include "catch.hpp"
+#include "../sender/Sender.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+Sender s;
+
+TEST_CASE("This is the First CASE")
+{
+    
+    string third = "10/23/45 23:45,No additional comments";
+    
+    
+    char data1 = s.PrintSelectedColumn(1,2,third,15);
+    cout<<data1<<endl;
+    char data = 'N';
+    REQUIRE(data1==data);
+}
+
