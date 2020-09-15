0a0b2368524a6453b8313d487a5f0bbeb1dfcb06
Chandan N <49569507+chandangowda7@users.noreply.github.com>
2020-09-15 13:23:38 +0530

    Update receiver.h

--- receiver/receiver.h
+++ receiver/receiver.h
@@ -5,7 +5,7 @@
 using namespace std;
 namespace Receiver
 {
- map<string, int> Receiver::RemoveStopWords(map<string, int>);
+ std::map<string, int> Receiver::RemoveStopWords(std::map<string, int>);
 std::map<std::string, int> countWords(std::map<std::string, int>&, string);
 void printwordandwordcounts(std::map<std::string, int>);
 std::map<string, int> InsertWords(std::map<string, int>, std::vector<string>);
