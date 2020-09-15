8f063205d2b7f28018aac3cf04c6c8b9bbe4ca1d
Chandan N <49569507+chandangowda7@users.noreply.github.com>
2020-09-15 13:18:39 +0530

    Update receiver.cpp

--- receiver/receiver.cpp
+++ receiver/receiver.cpp
@@ -1,13 +1,6 @@
 
 #include "receiver.h"
-
 using namespace std;
-
-/*map<std::string, int> countWords(map<std::string, int>&, string);
-void printwordandwordcounts(map<std::string, int>);
-map<string, int> InsertWords(map<string, int>, std::vector<string>);
-vector<string> checkforalphabets(string,vector<string>);*/
-
 int main()
 {
     map<string, int> M;
@@ -19,12 +12,11 @@ int main()
         map<string, int> tempMap = Receiver::countWords(M, line);
         M = tempMap;
     }
-    Receiver::printwordandwordcounts(M);
+    Receiver::printwordandwordcounts(Receiver::RemoveStopWords(M));
     return 0;
 }
 void Receiver::printwordandwordcounts(map<std::string, int> M)
 {
-
     if (M.size() > 0) {
         fstream fout;
         fout.open("sample-review/WordCount.csv", ios::out | ios::app);
@@ -38,12 +30,9 @@ void Receiver::printwordandwordcounts(map<std::string, int> M)
 }
 map<std::string, int> Receiver::countWords(map<std::string, int>& M, string line)
 {
-    // String for storing the words
     string word = "";
     std::vector<string> words,tempwords;
-
     for (int i = 0; line[i] != '\0'; i++) {
-
         if (line[i] == ' ') {
             tempwords=Receiver::checkforalphabets(word,words);
             words=tempwords;
@@ -65,7 +54,6 @@ map<string, int> Receiver::InsertWords(map<string, int> M, std::vector<string> w
         else
             M[word]++;
     }
-
     return M;
 }
 vector<string> Receiver::checkforalphabets(string word,vector<string> words)
@@ -75,9 +63,26 @@ while(word[i])
 {
     if (!isalpha(word[i]))
         return words;
-    i++;
-  
+    i++;  
 }
     words.push_back(word);
     return words;
+}
+map<string, int> Receiver::RemoveStopWords(map<string, int> M)
+{
+    map<string, int> tempMap=M;
+    vector<string> mostcommonstopwords={"i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"};
+    for (auto& iterator : M) {
+        std::vector<string>::iterator itr;i
+            itr=std::find(mostcommonstopwords.begin(),mostcommonstopwords.end(),iterator.first);
+        if(it!=mostcommonstopwords.end())
+        {
+            tempMap.erase(iterator.first);
+        }
+    }
+    return tempMap;
+        
+        
+        
+        
 }
