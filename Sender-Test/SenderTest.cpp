071c80b95d4ae013664a4bb39d8dc143f2e56aa1
RachotiBiradar <44144000+RachotiBiradar@users.noreply.github.com>
2020-09-15 18:46:16 +0530

    Update SenderTest.cpp

--- Sender-Test/SenderTest.cpp
+++ Sender-Test/SenderTest.cpp
@@ -23,6 +23,16 @@ bool CheckIfTheFileExists(string& FileName)
     }
     return exist;
 }
+bool isFileOpen(fstream& fin,string& fileName)
+{
+    fin.open(fileName.c_str(),ios::in);
+	if(!fin.is_open())
+    {
+        cout << "File cannot be opened" << endl;
+		
+    }
+    return !fin.is_open();
+}
 
 TEST_CASE("WHEN we pass to function a CSV line containing date & comment and if we need comment part THEN it returns Comment part")
 {    
@@ -36,17 +46,30 @@ TEST_CASE("WHEN we pass to function a CSV line containing date & comment and if 
     string return_string = s.CommaCounter(ExampleCSVString,0);    
     REQUIRE(return_string == "10/23/45 23:45");
 }
-TEST_CASE("WHEN we pass the filename to the function THEN it returns if the file exists or not")
+TEST_CASE("WHEN the given sentence does not contain a COMMA then it returns the whole string")
+{
+    string ExampleCSVString = "10/23/45 23:45No additional comments";    
+    string return_string = s.CommaCounter(ExampleCSVString,0);    
+    REQUIRE(return_string == "10/23/45 23:45No additional comments");
+}
+TEST_CASE("WHEN we pass the filename to the function THEN it returns TRUE")
 {
     string FileName = "sample-review/review-report.csv";
     bool FileExists = CheckIfTheFileExists(FileName);
     REQUIRE(FileExists == true);
 }
-TEST_CASE("WHEN we pass the filename to the function THEN it returns if the file  not")
+TEST_CASE("WHEN we pass the filename to the function THEN it returns FALSE")
 {
     string FileName = "sample-review/review-report1.csv";
     bool FileExists = CheckIfTheFileExists(FileName);
     REQUIRE(FileExists == false);
+}
+TEST_CASE("WHEN we pass the filename to the function THEN it returns the file is open")
+{
+    fstream fin;
+    string FileName = "sample-review/review-report.csv";
+    bool ISFILEOPEN = isFileOpen(fin,FileName);
+    REQUIRE(ISFILEOPEN == false);
 }
 
  
