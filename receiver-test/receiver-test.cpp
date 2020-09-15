a3836a5267fbe16335b42814d835e7f50dd214ee
Chandan N <49569507+chandangowda7@users.noreply.github.com>
2020-09-15 11:42:38 +0530

    Update receiver-test.cpp

--- receiver-test/receiver-test.cpp
+++ receiver-test/receiver-test.cpp
@@ -10,11 +10,33 @@ TEST_CASE("printing the word and word count") {
  map<std::string, int> M;
    M.insert(make_pair("one", 1));
   std::vector<string> words={"one","two"};
-  std::map<std::string, int> result,res2;
+  std::map<std::string, int> result;
   result.insert(make_pair("one", 2));
   result.insert(make_pair("two", 1));
-  res2=Receiver::InsertWords(M,words);
-  
-    REQUIRE(res2==result);
+ 
+    REQUIRE(Receiver::InsertWords(M,words)==result);
     
+}
+TEST_CASE("checking if word contains alphabets or not")
+{
+ vector<string> words={"one","comment"};
+
+REQUIRE(Receiver::checkforalphabets("79NOSL",words)==words);
+}
+TEST_CASE("disintergare the line and add each words to the map")
+{
+ 
+  map<std::string, int> M;
+  M.insert(make_pair("one", 1));
+ M.insert(make_pair("two", 2));
+ string line="one of these is not required";
+ map<std::string, int> result;
+ result.insert(make_pair("one", 2));
+ result.insert(make_pair("two", 2));
+ result.insert(make_pair("of", 1));
+ result.insert(make_pair("these", 1));
+ result.insert(make_pair("is", 1));
+ result.insert(make_pair("not", 1));
+ result.insert(make_pair("required", 1));
+ REQUIRE( Receiver::countWords(M,line)=result);
 }
