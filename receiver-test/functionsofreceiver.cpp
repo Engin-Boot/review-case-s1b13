b78afced1200eb8ab09932dd4030067d926b247f
Chandan N <49569507+chandangowda7@users.noreply.github.com>
2020-09-15 13:44:05 +0530

    Update functionsofreceiver.cpp

--- receiver-test/functionsofreceiver.cpp
+++ receiver-test/functionsofreceiver.cpp
@@ -1,12 +1,8 @@
 #include "../receiver/receiver.h"
-
+#include<algorithm>
 using namespace std;
-
-
-
 void Receiver::printwordandwordcounts(map<std::string, int> M)
 {
-
     if (M.size() > 0) {
         fstream fout;
         fout.open("sample-review/WordCount.csv", ios::out | ios::app);
@@ -61,4 +57,19 @@ while(word[i])
 }
     words.push_back(word);
     return words;
+}
+map<string, int> Receiver::RemoveStopWords(map<string, int> M)
+{
+    map<string, int> tempMap=M;
+    vector<string> mostcommonstopwords={"i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"};
+    for (auto& iterator : M) {
+        std::vector<string>::iterator itr;
+        string searchkey=(string)iterator.first;
+            itr=std::find(mostcommonstopwords.begin(),mostcommonstopwords.end(),searchkey);
+        if(itr!=mostcommonstopwords.end())
+        {
+            tempMap.erase(iterator.first);
+        }
+    }
+    return tempMap;       
 }
